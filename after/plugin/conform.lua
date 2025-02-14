265d13cf9b6eebf8cc27147791d086ebaf541008
KennethLG <orsonleiziaga@gmail.com>
2025-02-14 16:20:24 -0500

    disable autoformatting

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -7,11 +7,11 @@ require("conform").setup({
 		json = { "prettier" },
 		lua = { "stylua" },
 	},
-	format_on_save = {
-		timeout_ms = 500,
-		-- lsp_fallback = true,
-		lsp_format = "fallback",
-	},
+	-- format_on_save = {
+	-- 	timeout_ms = 500,
+	-- 	-- lsp_fallback = true,
+	-- 	lsp_format = "fallback",
+	-- },
 	notify_on_error = true, -- Notify if formatting fails
 })
 
@@ -19,9 +19,9 @@ vim.keymap.set("n", "<leader>F", function()
 	require("conform").format({ async = true, lsp_fallback = true })
 end, { noremap = true, silent = true })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = "*",
+-- 	callback = function(args)
+-- 		require("conform").format({ bufnr = args.buf })
+-- 	end,
+-- })
